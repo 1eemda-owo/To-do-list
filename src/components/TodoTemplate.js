901ec31f986b741a3591c1776a66cd4668eeb8bb
1eemda_owo <philoe2455@naver.com>
2022-07-25 18:48:13 +0900

    todo

---
+++ src/components/TodoTemplate.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../style/TodoTemplate.css";
+
+const TodoTemplate = () => {
+  return (
+    <div className="Template">
+      <div className="app-title">일정 관리</div>
+      <div className="content">test</div>
+    </div>
+  );
+};
+
+export default TodoTemplate;
